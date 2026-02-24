f4198349e75aed370452fa620d11bd74c24c5adc
J.A. Oseguera <jose.oseguera@emergys.com>
2026-02-24 15:14:46 -0500

    initial commit

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+      <h1>Hello, React! 👋</h1>
+    </main>
+  );
+}
